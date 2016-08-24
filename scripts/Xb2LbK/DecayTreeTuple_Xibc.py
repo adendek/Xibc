f957131ccc5824a23877e1bd337530be79c6ad60
Adam Dendek <adendek@nat110-dyn-235.fis.agh.edu.pl>
2016-08-24 13:20:07 +0200

    update Xb2LbK DecayTreeTuple

--- scripts/Xb2LbK/DecayTreeTuple_Xibc.py
+++ scripts/Xb2LbK/DecayTreeTuple_Xibc.py
@@ -8,7 +8,14 @@ from Configurables import LoKi__Hybrid__EvtTupleTool as LoKiTool
 
 
 #######################################
-tuple = DecayTreeTuple('xibc')
+# file created by Adam Dendek
+# http://cern.ch/go/8Bds
+#
+# I based on file Xb2LbPi/DecayTreeTuple_Xibc.py
+#
+#######################################
+
+tuple = DecayTreeTuple('lambdab')
 tuple.Inputs = ["/Event/BhadronCompleteEvent/Phys/Lb2LcKLc2PKPiBeauty2CharmLine/Particles"]
 tuple.TupleName = "mytree"
 tuple.ToolList = [
@@ -19,10 +26,6 @@ tuple.ToolList = [
        "TupleToolPid",
        "TupleToolTrackInfo",
        "TupleToolPropertime",
-       #
-       "TupleToolMCTruth"
-       #"TupleToolMCBackgroundInfo",
-       #
        ]
 tistos = TupleToolTISTOS("tistos")
 tistos.VerboseL0   = 1
@@ -41,44 +44,39 @@ tistos.TriggerList = [
     'Hlt2Topo4BodyBBDTDecision',
     'Hlt2Topo4BodySimpleDecision',
     ]
-
-LoKi_xib = LoKi__Hybrid__TupleTool("LoKi_xib")
-LoKi_xib.Variables =  {
+LoKi_lab0 = LoKi__Hybrid__TupleTool("LoKi_lab0")
+LoKi_lab0.Variables =  {
     #"LOKI_ETA"             : "ETA",
-    "LV01"                 : "LV01",
-    "LV02"                 : "LV02",
-    "LOKI_DIRA"            : "BPVDIRA" ,
+    #"LV01"                 : "LV01",
+    #"LV02"                 : "LV02",
+    #"LOKI_DIRA"            : "BPVDIRA" ,
     "LOKI_DTF_CTAU"        : "DTF_CTAU( 0, True )",
-    "LOKI_FDCHI2"           : "BPVVDCHI2",
-    "LOKI_BPVIPCHI2"        : "BPVIPCHI2()",
+    "LOKI_FDCHI2"          : "BPVVDCHI2",
+    #"LOKI_BPVIPCHI2"       : "BPVIPCHI2()",
     "LOKI_MASS_LcConstr"        : "DTF_FUN ( M ,                  True , 'Lambda_c+')",
     "LOKI_VCHI2NDOF_LcConstr"   : "DTF_FUN ( VFASPF(VCHI2/VDOF) , True , 'Lambda_c+')",
-    "LOKI_CHI2NDOF_LcConstr"    : "DTF_CHI2NDOF( True,'Lambda_c+')",
+    "LOKI_CHI2NDOF_LcConstr"    : "DTF_CHI2NDOF( True,'Lambda_c+')"
     }
 
-
 #######################################
-tuple.Decay = "[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC"
+tuple.Decay = '[ Lambda_b0 -> ^(Lambda_c+ -> ^p+ ^K- ^pi+) ^K- ]CC'
 tuple.Branches = {
-    "Xb"   :   "[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC",
-    "Pi"    :   "[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC",
-    "Lc"    :   "[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC",
-    "p"       :   "[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC",
-    "k"       :   "[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC",
-    "pi"      :   "[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC"
-   }
+    "Xb"   :  "[ Lambda_b0 -> (Lambda_c+ -> p+ K- pi+) K- ]CC",
+    "Xc"   :  "[ Lambda_b0 -> ^(Lambda_c+ -> p+ K- pi+) K- ]CC",
+    "p"      :  "[ Lambda_b0 -> (Lambda_c+ -> ^p+ K- pi+) K- ]CC",
+    "k"      :  "[ Lambda_b0 -> (Lambda_c+ -> p+ ^K- pi+) K- ]CC",
+    "pi"     :  "[ Lambda_b0 -> (Lambda_c+ -> p+ K- ^pi+) K- ]CC",
+    "XcK"   :  "[ Lambda_b0 -> (Lambda_c+ -> p+ K- pi+) ^K- ]CC"
+    }
 #######################################
 tuple.addTool(TupleToolDecay, name="Xb")
-tuple.Lb.addTool(tistos)
-tuple.Lb.ToolList+=[ "TupleToolPropertime" ]
-tuple.Lb.ToolList+=["TupleToolTISTOS/tistos"]
-#tuple.lab2.addTool(tistos)
-#tuple.lab2.ToolList+=["TupleToolTISTOS/tistos"]
-#tuple.addTool(LoKi_track)
-#tuple.ToolList += [ "LoKi::Hybrid::TupleTool/LoKi_track" ]
+tuple.addTool(TupleToolDecay, name="Xc")
+tuple.Xb.addTool(tistos)
+tuple.Xb.ToolList+=[ "TupleToolPropertime" ]
+tuple.Xb.ToolList+=["TupleToolTISTOS/tistos"]
 #######################################
-tuple.Lb.addTool(LoKi_lab0)
-tuple.Lb.ToolList+=["LoKi::Hybrid::TupleTool/LoKi_xib"]
+tuple.Xb.addTool(LoKi_lab0)
+tuple.Xb.ToolList+=["LoKi::Hybrid::TupleTool/LoKi_lab0"]
 tuple.addTool(LoKiTool , 'MyLoKiTool' )
 tuple.ToolList += [ "LoKi::Hybrid::EvtTupleTool/MyLoKiTool" ]
 #######################################
@@ -94,21 +92,5 @@ tuple.ReFitPVs = True
 #######################################
 
 
-'''
-May be I need that kind of code too. But right now I am not sure.
-#######################################
-tuplews = tupleplus.clone("wsxibc")
-tuplews.Decay = "[ Xi_bc~- -> ^pi- ^(Lambda_b0 -> ^(Lambda_c+ -> ^p+ ^K- ^pi+) ^pi- ) ]CC"
-tuplews.Branches = {
-    "Xb"   :  "[ Xi_bc~- -> pi- (Lambda_b0 -> (Lambda_c+ -> p+ K- pi+) pi- ) ]CC",
-    "Pi"    :  "[ Xi_bc~- -> ^pi- (Lambda_b0 -> (Lambda_c+ -> p+ K- pi+) pi- ) ]CC",
-    "Lb"    :  "[ Xi_bc~- -> pi- ^(Lambda_b0 -> (Lambda_c+ -> p+ K- pi+) pi- ) ]CC",
-    "Lc"    :  "[ Xi_bc~- -> pi- (Lambda_b0 -> ^(Lambda_c+ -> p+ K- pi+) pi- ) ]CC",
-    "p"       :  "[ Xi_bc~- -> pi- (Lambda_b0 -> (Lambda_c+ -> ^p+ K- pi+) pi- ) ]CC",
-    "k"       :  "[ Xi_bc~- -> pi- (Lambda_b0 -> (Lambda_c+ -> p+ ^K- pi+) pi- ) ]CC",
-    "pi"      :  "[ Xi_bc~- -> pi- (Lambda_b0 -> (Lambda_c+ -> p+ K- ^pi+) pi- ) ]CC",
-    "LbPi"    :  "[ Xi_bc~- -> pi- (Lambda_b0 -> (Lambda_c+ -> p+ K- pi+) ^pi- ) ]CC",
-    }
-#######################################
-'''
-
+############### the similar stuff for wrong side study
+############## but right now I skipp this step. ss
--- scripts/Xb2LbK/RunXb2LcK.py
+++ scripts/Xb2LbK/RunXb2LcK.py
@@ -13,27 +13,58 @@ scaler = TrackScaleState( 'StateScale' )
 
 kaons        = DataOnDemand(Location = "Phys/StdAllNoPIDsKaons/Particles")
 protons      = DataOnDemand(Location = "Phys/StdAllNoPIDsProtons/Particles")
-pions    = DataOnDemand(Location = "Phys/StdAllNoPIDsPions/Particles")
+longpions    = DataOnDemand(Location = "Phys/StdAllNoPIDsPions/Particles")
+
+
+
+#-------------------------------------------------------------------------------------------------
+locationLb   = "/Event/BhadronCompleteEvent/Phys/Phys/Lb2LcKLc2PKPiBeauty2CharmLine/Particles"
+if simulation:
+    locationLb = "/Event/AllStreams/Phys/Lb2LcKLc2PKPiBeauty2CharmLine/Particles"
+Lb2LcK      = AutomaticData(Location = locationLb)
+
+_MyXb2XcK = FilterDesktop('_MyXb2XcK')
+_MyXb2XcK.Code = "(MAXTREE('p+'==ABSID,PROBNNp)>0.1)&(MAXTREE('K+'==ABSID,PROBNNk)>0.05)&(MAXTREE('Lambda_b0'==ABSID,M)>5350*MeV)&(MAXTREE('Lambda_b0'==ABSID,M)<6000*MeV)&(MAXTREE('Lambda_c+'==ABSID, M)>2260*MeV)&(MAXTREE('Lambda_c+'==ABSID, M)<2315*MeV)"
+MyLb2Lcpi = Selection("MyLb2Lcpi", Algorithm = _MyXb2XcK, RequiredSelections = [Lb2LcK] )
+#-------------------------------------------------------------------------------------------------
+
+
+#BPVVDCHI2: chi2 separation from related PV
+#BPVIPCHI2(): Computes the chi2-IP on the related PV
 
 
 #----Selection Xibb -> /\b pi+/-----------------------------------------------------------------
-_Sigb2LcK = CombineParticles( "_Sigb2LcK",
-                              DecayDescriptors = ["[ Xi_bc+ -> ^K- ^(Lambda_c+ -> ^p+ ^K- ^pi+) ]CC"],
+_Sigb2Lbpi = CombineParticles( "_Sigb2Lbpi",
+                              DecayDescriptors = ["[Xi_bc+ -> Lambda_b0 pi+]cc"],
                               CombinationCut   = "(AM-AM1-AM2>450*MeV) & (APT>2*GeV)",
-                              DaughtersCuts    = { "K-"      : "(PT > 200*MeV) & (P > 2000*MeV) & (TRCHI2DOF < 4.0) & (MIPCHI2DV(PRIMARY)>4)"},
-                              MotherCut        = "(M-M1-M2>500*MeV) & (PT>1*GeV) & (VFASPF(VCHI2/VDOF)<10) & (MIPCHI2DV(PRIMARY)<16)", 
+                              DaughtersCuts    = { "pi+"      : "(PT > 200*MeV) & (P > 2000*MeV) & (TRCHI2DOF < 4.0) & (MIPCHI2DV(PRIMARY)>4)"},
+                              MotherCut        = "(M-M1-M2>500*MeV) & (PT>1*GeV) & (VFASPF(VCHI2/VDOF)<10) & (MIPCHI2DV(PRIMARY)<16)",
                               ReFitPVs         = True )
 Sigb2Lbpi  = Selection( "Sigb2Lbpi ",
-                      Algorithm          = _Sigb2LcK ,
-                      RequiredSelections = [ MyLb2LbK,  kaons] )
+                      Algorithm          = _Sigb2Lbpi ,
+                      RequiredSelections = [ MyLb2Lcpi,  longpions] )
+
+_wsSigb2Lbpi = CombineParticles( "_wsSigb2Lbpi",
+                              DecayDescriptors = ["[Xi_bc+ -> Lambda_b~0 pi+]cc"],
+                              CombinationCut   = "(AM-AM1-AM2>450*MeV) & (APT>2*GeV)",
+                              DaughtersCuts    = { "pi+"      : "(PT > 200*MeV) & (P > 2000*MeV) & (TRCHI2DOF < 4.0) & (MIPCHI2DV(PRIMARY)>4)"},
+                              MotherCut        = "(M-M1-M2>500*MeV) & (PT>1*GeV) & (VFASPF(VCHI2/VDOF)<10) & (MIPCHI2DV(PRIMARY)<16)",
+                              ReFitPVs         = True )
+wsSigb2Lbpi  = Selection( "wsSigb2Lbpi ",
+                      Algorithm          = _wsSigb2Lbpi ,
+                      RequiredSelections = [ MyLb2Lcpi,  longpions] )
 
 
 #-------------------------------------------------------------------------------------------------
 
 
 ### Gaudi sequences ----------------------------------------------
-SeqSigb2LcK = SelectionSequence("SeqSigb2LcK", TopSelection = MyLb2LbK)
-seqXib = SeqSigb2LcK.sequence()
+SeqLb2Lcpi = SelectionSequence("SeqLb2Lcpi", TopSelection = MyLb2Lcpi)
+seqLb = SeqLb2Lcpi.sequence()
+SeqSigb2Lbpi = SelectionSequence("SeqSigb2Lbpi", TopSelection = Sigb2Lbpi)
+seqXib = SeqSigb2Lbpi.sequence()
+wsSeqSigb2Lbpi = SelectionSequence("wsSeqSigb2Lbpi", TopSelection = wsSigb2Lbpi)
+wsseqXib = wsSeqSigb2Lbpi.sequence()
 #---------------------------------------------------------------
 
 
@@ -47,8 +78,8 @@ from Configurables import  DecayTreeTuple, MCDecayTreeTuple
 importOptions("DecayTreeTuple_Xibc.py")
 
 from PhysConf.Filters import LoKi_Filters
-fltrs = LoKi_Filters (STRIP_Code = " HLT_PASS_RE('StrippingLb2LcKLc2PKPiBeauty2CharmLine') ")
-fltrSeq = fltrs.sequence ( 'MyFilters' )  
+fltrs = LoKi_Filters (STRIP_Code = " HLT_PASS_RE('StrippingLb2LcPiNoIPLc2PKPiBeauty2CharmLineDecision') ")
+fltrSeq = fltrs.sequence ( 'MyFilters' )
 
 from Configurables import LoKi__HDRFilter   as StripFilter
 if simulation == False:
@@ -56,16 +87,22 @@ if simulation == False:
 
 
 tuple0  = DecayTreeTuple( "xibc" )
-tuple0.Inputs = [ SeqSigb2LcK.outputLocation() ]
+tuple0.Inputs = [ SeqSigb2Lbpi.outputLocation() ]
+tuple1  = DecayTreeTuple( "wsxibc")
+tuple1.Inputs = [ wsSeqSigb2Lbpi.outputLocation() ]
+tupleLb = DecayTreeTuple( "lambdab" )
+tupleLb.Inputs = [ SeqLb2Lcpi.outputLocation() ]
 
 
 if simulation:
     tuple0.ToolList += [ "TupleToolMCTruth" ]
+    tuple1.ToolList += [ "TupleToolMCTruth" ]
+    tupleLb.ToolList += [ "TupleToolMCTruth" ]
 
 
 
 #DaVinci().appendToMainSequence( [scaler] )
-DaVinci().UserAlgorithms = [ scaler, seqXib, tuple0]
+DaVinci().UserAlgorithms = [ scaler, seqLb, seqXib, wsseqXib, tuple0, tuple1, tupleLb]
 
 #
 DaVinci().EvtMax     = -1
